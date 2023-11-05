184717772453492544abdb9f86df1cf5f42845ef
Gabriel MP <contato.gabrielmartinspadoin@gmail.com>
2023-11-05 20:59:37 -0300

    [VUE] DATA_AND_METHODS - feat: learning a little more about data and methods in vue v2

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 new Vue({
   el: "#vue-app",
   data: {
-    name: "Gabriel"
+    name: "Gabriel",
+    job: "Ninja"
+  },
+  methods: {
+    greet: function(timeOfDay) {
+      return `Good ${timeOfDay}, ${this.name}`
+    }
   }
 })
