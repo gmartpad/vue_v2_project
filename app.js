76d6949a7d39b9e2e3c67d72adea494045623a4d
Gabriel MP <contato.gabrielmartinspadoin@gmail.com>
2023-11-05 23:25:59 -0300

    [VUE] EVENTS - feat: implementing some event detection with vue v2

--- app.js
+++ app.js
@@ -4,11 +4,24 @@ new Vue({
     name: "Gabriel",
     job: "Ninja",
     website: "https://www.google.com",
-    websiteTag: '<a href="https://www.google.com">Google Tag</a>'
+    websiteTag: '<a href="https://www.google.com">Google Tag</a>',
+    age: 23,
+    x: 0,
+    y: 0
   },
   methods: {
     greet: function(timeOfDay) {
       return `Good ${timeOfDay}, ${this.name}`
+    },
+    add: function(inc) {
+      this.age += inc
+    },
+    subtract: function(dec) {
+      this.age -= dec
+    },
+    updateXY: function(event) {
+      this.x = event.offsetX
+      this.y = event.offsetY
     }
   }
 })
